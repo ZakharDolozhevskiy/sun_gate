f5e59284c23b847f0d5818325948c53dd5623457
unknown <Zakhar_Dolozhevskyi@EPUAKHAW0153.kyiv.epam.com>
2015-02-12 18:23:44 +0200

    modify structure as a jQuery plugin

--- textEditor/js/editor.js
+++ textEditor/js/editor.js
@@ -5,88 +5,101 @@
      * @constructor
      */
     var Editor = function(container){
-        this.container = container;
+        Editor.prototype.container = container;
         Editor.prototype.init(container);
     };
 
     $.extend(Editor.prototype, {
 
         init: function(container) {
-            var iframe = $('<iframe/>').addClass('content'),
-                $content = $('<div/>').addClass('content'),
-                iWindow, iDoc;
-            $(container).append(iframe);
+            // render UI components
+            this.render(container);
 
-            // save iframe's window object link
-            iWindow = iframe[0].contentWindow;
-            iDoc = iframe[0].contentDocument;
+            // init the library that work with user's selection
+            rangyCreate(this.iWindow);
 
-            // added content into iframe and make it editable
-            $content.attr('contenteditable','true')
+            this.rangy = this.iWindow.rangy;
+
+            this.rangy.init();
+
+
+            // add style toggles
+
+            this.controls = {
+                bold: this.rangy.createCssClassApplier("bold", true),
+                italic: this.rangy.createCssClassApplier("italic", true),
+                underLine: this.rangy.createCssClassApplier("underline", true)
+            };
+        },
+
+        render: function(container){
+            var $iframe = $('<iframe/>').addClass('content'),
+                $content = $('<div/>').addClass('content')
+                    .attr('contenteditable','true')
                     .attr('ondragstart','return false')
+                    .css('min-height','100%')
                     .text('test string');
 
-            $(iDoc).find('body').append($content);
+            $(container).append($iframe);
+
+            // save iframe's window object link to Editor prototype
+
+            this.iWindow = $iframe[0].contentWindow;
+            this.iDoc = $iframe[0].contentDocument;
+
+            // add content into iframe window
+
+            $(this.iDoc).find('body').append($content);
+
+            // init a legend in the user container
 
             this.createLegend(container);
-       },
+        },
 
-       createLegend: function(container) {
-           var elem = $('<div/>').addClass('legend');
+        createLegend: function(container) {
+           var elem = $('<div/>').addClass('legend'),
+               // add preview button
+               preview = $('<button/>')
+                       .text('Show preview')
+                       .addClass('preview')
+                       .on('click', $.proxy(this.showPreview, this)),
+                // test btn
+               $b_tgl = $('<button/>').text('bold').on('click', $.proxy(this.setBold, this)),
+               $i_tgl = $('<button/>').text('italic').on('click', $.proxy(this.setItalic, this)),
+               $u_tgl = $('<button/>').text('underline').on('click', $.proxy(this.setUnderline, this));
 
-           $(container).append(elem);
-       }
 
+            $(container).append(elem)
+                        .append(preview)
+                        .append($b_tgl)
+                        .append($i_tgl)
+                        .append($u_tgl);
+        },
+
+        showPreview: function(){
+            var newWin = open("", "Preview", "height=600,width=800"),
+                // content that have been added in the editable iframe's window
+               content= $(this.iDoc.body).html();
+
+            $(newWin.document.body).html( content );
+        },
+
+        // ---------- functions - controls block --------------
+
+        setBold: function() {
+            debugger;
+            this.controls.bold.toggleSelection();
+        },
+
+        setItalic: function() {
+            this.controls.italic.toggleSelection();
+        },
+
+        setUnderline: function () {
+            this.controls.underLine.toggleSelection();
+        }
 
     });
-
-    //Editor.prototype = {
-    //    controls : {},
-    //
-    //    init: function(){
-    //        rangy.init();
-    //
-    //        this.controls = {
-    //            bold: rangy.createCssClassApplier("bold", true),
-    //            italic: rangy.createCssClassApplier("italic", true),
-    //            underLine: rangy.createCssClassApplier("underline", true)
-    //        };
-    //
-    //
-    //    },
-    //
-    //    setBold: function() {
-    //        this.controls.bold.toggleSelection();
-    //    },
-    //
-    //    setItalic: function() {
-    //        this.controls.italic.toggleSelection();
-    //    },
-    //
-    //    setUnderline: function () {
-    //        this.controls.underLine.toggleSelection();
-    //    },
-    //
-    //    previewer: function (flag) {
-    //        if(flag){
-    //            var params = 'scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no' +
-    //                'width=900,height=900,left=100,top=100',
-    //                win;
-    //
-    //                win = window.open('http://localhost:63342/textEditor/frames/testFrame.html', 'preview', params);
-    //
-    //                $(win).on("message", function(e) {
-    //                    var content = JSON.parse(e.originalEvent.data);
-    //                    $(win.document).find('#content').html(content);
-    //                });
-    //        }
-    //        return win;
-    //    },
-    //
-    //    getContent: function() {
-    //        return this.container.html();
-    //    }
-    //};
 
     new Editor($('#editor'));
 
