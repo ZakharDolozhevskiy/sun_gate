d81d12feee6b2c1e486ec952218261307b135358
Zakhar_Dolozhevskyi <dolozhevskiy@gmail.com>
2015-04-22 18:07:25 +0300

    routing have been added

--- FoursquareApp/app/js/app.js
+++ FoursquareApp/app/js/app.js
@@ -1,15 +1,6 @@
 (function() {
     "use strict";
 
-    var app = angular.module('foursquareApp',['ngResource','ngAnimate']);
-
-        app.constant('appConfig', {
-            api: {
-                baseUrl: 'https://api.foursquare.com/v2/venues/search',
-                clientId: '3VGP01AEP2PMURE2AYFGUUOUX2LBXZLW55YR5B01AYPVL0DM',
-                clientSecret: 'BMU5LDZR3UP2XUYRZNRQZMYMHJ23RSLM1IOJQ5Z0NCKVZEZ4',
-                version: '20130815'
-            }
-        });
+    var app = angular.module('foursquareApp',['ngResource','ngAnimate', 'ui.router']);
 
 })();
