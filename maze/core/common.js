1eaa2e241f49943a9635413cc9c8ef90a15c7000
Zakhar_PC <dolozhevskiy@gmail.com>
2015-05-18 21:17:37 +0300

    maze solution final version

--- maze/core/common.js
+++ maze/core/common.js
@@ -1,8 +1,8 @@
 (function ($) {
   "use strict";
-  var canvas, ctx, img, imgPixels, model, paintOne,
-      modelFormer, mazeCellProcessor, passMaze, MAZE_W,
-      WALL, PROCCESED, EMPTY, COLOR, MAZE_H;
+  var canvas, ctx, img, imgPixels, model, paintOne, attachLoader,
+      modelFormer, mazeCellProcessor, passMaze, start_btn, oneHandAlgorithm,
+      MAZE_W, WALL, PROCESSED, EMPTY, COLOR, MAZE_H;
   /**
    * Constants
    * Canvas element width and height
@@ -13,10 +13,16 @@
   MAZE_H = 601;
   WALL = 1;
   EMPTY = 0;
-  PROCCESED = 3;
-  COLOR = '0,211,96,204'.split(',');
+  PROCESSED = 2;
+  COLOR = [0, 211, 96, 204]; // RGBa format
   model = [];
 
+  /**
+   * Algorithm that check neighbors and call again if neighbors have EMPTY
+   * no processed item
+   * @param x integer
+   * @param y integer
+   */
   mazeCellProcessor = function (x, y) {
     var k = 0;
 
@@ -27,10 +33,10 @@
       if (model[x + 1][y] !== EMPTY) { k++; }
       if (model[x][y + 1] !== EMPTY) { k++; }
 
-      if (k === 4) { model[x][y] = PROCCESED; }
+      if (k === 4) { model[x][y] = PROCESSED; }
 
       if (k === 3) {
-        model[x][y] = PROCCESED;
+        model[x][y] = PROCESSED;
 
         if (model[x - 1][y] === EMPTY) { mazeCellProcessor(x - 1, y); }
         if (model[x][y - 1] === EMPTY) { mazeCellProcessor(x, y - 1); }
@@ -39,29 +45,49 @@
       }
     }
   };
-
+  /**
+   * Processed data with colors and form model
+   * @param data Array with pixels colors in RGBa format
+   */
   modelFormer = function (data) {
     var t = [], i;
     for (i = 0; i < data.length; i += 4) {
       data[i] === 255 ? t.push(EMPTY) : t.push(WALL);
-
+      // Row should't be larger then maze width
       if (t.length + 1 > MAZE_W) {
         model.push(t);
         t = [];
       }
     }
   };
-
+  /**
+   * Processed model and render the solution way on the image
+   */
   passMaze = function () {
     var x, y;
-
-    for (x = 1; x < model.length - 1; x++) {
-      for (y = 1; y < model[x].length - 1; y++) {
-        mazeCellProcessor(x, y);
+    // Adding loader to the button
+    attachLoader(true);
+    setTimeout(function () {
+      for (x = 1; x < model.length - 1; x++) {
+        for (y = 1; y < model[x].length - 1; y++) {
+          mazeCellProcessor(x, y);
+        }
       }
-    }
+      //apply model changes to view
+      model.forEach(function (row, i) {
+        row.forEach(function (pxl, j) {
+          pxl === EMPTY ? paintOne(j, i) : false;
+        });
+      });
+      // Detach loader img after maze prossed
+      attachLoader();
+    }, 100);
   };
-
+  /**
+   * Get pixel in the page and apply the color in RGBa format
+   * @param x integer
+   * @param y integer
+   */
   paintOne = function (x, y) {
     var pxl;
     pxl = ctx.getImageData(x, y, 1, 1);
@@ -71,11 +97,20 @@
     pxl.data[3] = COLOR[3];  // a
     ctx.putImageData(pxl, x, y);
   };
-
+  /**
+   * Loader image attach function
+   */
+  attachLoader = function(flag) {
+    flag ? start_btn.addClass('action').text(' ') :
+           start_btn.removeClass('action').text('Done').attr('disabled', 'disabled');
+  };
   //Render img from jpg format to canvas
   img =  $('<img/>').attr('src', "imgs/maze.jpg")[0];
+  start_btn = $('.start');
   canvas = $('#maze');
+  // Get canvas context and add event listener to init button
   ctx = canvas[0].getContext("2d");
+  start_btn.click(passMaze);
   /**
    * After original image load start maze's image
    * pixels process and build data model.
@@ -85,12 +120,7 @@
     ctx.drawImage(img, 0, 0, MAZE_W, MAZE_H);
     imgPixels = ctx.getImageData(0, 0, MAZE_W, MAZE_H);
     modelFormer(imgPixels.data);
-    passMaze();
-
-    for (var x = 0; x < model.length; x++) {
-      for (var y = 0; y < model[x].length; y++) {
-        model[x][y] === EMPTY ? paintOne(x, y) : false;
-      }
-    }
+    // show init button
+    start_btn.show();
   };
 })(jQuery);
