87ba53ae7fe5207ec6f1a592ab1c661040292c78
Zakhar_PC <dolozhevskiy@gmail.com>
2015-07-14 22:52:27 +0300

    fixed gulp file

---
+++ ui-kit/README.md
@@ -0,0 +1,25 @@
+
+## How to run
+
+#### 1. Clone this repo:
+
+```sh
+https://github.com/ZakharDolozhevskiy/sun_gate.git
+```
+#### 2. Install gulp globally:
+
+```sh
+$ npm install --global gulp
+```
+
+#### 3. Install project dependencies:
+
+```sh
+$ npm install
+```
+
+#### 4. Run default task:
+
+```sh
+$ gulp
+```
--- ui-kit/gulpfile.js
+++ ui-kit/gulpfile.js
@@ -1,40 +1,69 @@
-var path,
+var path, conf,
     gulp = require('gulp'),
     less = require('gulp-less'),
     concat = require('gulp-concat'),
-    postcss = require('gulp-postcss'),
+    notify = require('gulp-notify'),
+    cached = require('gulp-cached'),
+    remember = require('gulp-remember'),
     browserSync =  require('browser-sync'),
     sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('autoprefixer-core');
+    spritesmith = require('gulp.spritesmith'),
+    autoprefixer = require('gulp-autoprefixer');
 
 path = {
-        less: ['layout styles/*.less', 'styles/**/*.less']
-    };
+  less: ['layout styles/*.less', 'styles/**/*.less'],
+  icons: ['./images/*'],
+  build: ['./build/styles/*.css', './build/*.html']
+};
+
+conf = {
+  sprite: {
+    imgName: 'i/sprite.png',
+    cssName: 'i/sprite.css',
+    imgPath: './images'
+  }
+};
+
+function errorHandler(err) {
+  notify.onError(function (err) {
+    return "Error: " + err.message;
+  }).call(this, err);
+
+  this.end();
+}
 
 gulp.task('server', function () {
-  var files = [
-    '..build/styles/*.css',
-    './index.html'
-  ];
-  browserSync.init(files,{
+  browserSync.init( path.build, {
     server: {
-      baseDir: './'
+      baseDir: ['./build', './'],
+      index: './build/index.html'
     }
   });
 });
 
-gulp.task('less_compile', function() {
+gulp.task('compile_less', function() {
   gulp.src(path.less)
     .pipe(sourcemaps.init())
-      .pipe(less({paths: ['styles/']}))
-      .pipe(concat('main.css'))
-      .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+    .pipe(cached())
+    .pipe(less({paths: ['styles/']}))
+    .on('error', errorHandler)
+    .pipe(autoprefixer(['last 2 version']))
+    .pipe(remember())
+    .pipe(concat('main.css'))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./build/styles'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(path.less, ['less_compile']);
+    gulp.watch(path.less, ['compile_less']);
 });
 
-gulp.task('default',['server','watch','less_compile']);
+gulp.task('sprite', function () {
+  var spriteData = gulp.src(path.icons)
+    .pipe(spritesmith(conf.sprite));
+
+  return spriteData.pipe(gulp.dest('./build'));
+});
+
+gulp.task('default',['compile_less', 'server', 'watch']);
+
